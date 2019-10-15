e958974cf705fc779b69c15144b0852a478f9e4a
Mehmet Murat Celiktas <mehmetmuratceliktas@Mehmets-MacBook-Pro-2.local>
2019-10-15 11:00:36 +0100

    logo

--- src/components/HeaderLogo.js
+++ src/components/HeaderLogo.js
@@ -6,7 +6,7 @@ const HeaderLogo = () => {
     <div className="logo-container">
       <img
         className="logo-img"
-        src="./images/logo.png"
+        src={require("../images/logo.png")}
         alt="logo"
         id="header-img"
       />
