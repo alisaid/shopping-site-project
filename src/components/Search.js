773f2ef9c70732523c3c0e73219c842781bfd321
Mehmet Murat Celiktas <mehmetmuratceliktas@Mehmets-MacBook-Pro-2.local>
2019-10-15 11:49:56 +0100

    v2.1

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { setSearchText, getSearchResult } from "../store/actions/search";
 
