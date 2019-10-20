573ed18d52d52b31f46adba7cb43fb7c1106a7a6
ahmet-serdar <nerelerdeyim505@gmail.com>
2019-10-20 22:03:41 +0200

    quantity selection

--- src/pages/ShoppingCartPage.js
+++ src/pages/ShoppingCartPage.js
@@ -1,46 +1,73 @@
 import React from "react";
 import { connect } from "react-redux";
 import QuantityForm from "../components/QuantityForm";
-import { addToCart } from "../store/actions/cart";
+import { addToCart, removeFromCart } from "../store/actions/cart";
+import BuyNow from "../components/BuyNow";
 
 const ShoppingCartPage = props => {
-  let myOrderedArray = props.data.reduce(function(accumulator, currentValue) {
+  const myOrderedArray = props.data.reduce(function(accumulator, currentValue) {
     if (accumulator.indexOf(currentValue) === -1) {
-      currentValue.quantity = 1;
       accumulator.push(currentValue);
-    } else {
-      currentValue.quantity = currentValue.quantity + 1;
     }
     return accumulator;
   }, []);
 
+  const totalPrice = myOrderedArray.reduce((acc, curVal) => {
+    const price = Number(curVal.price);
+    const itemTotalPrice = price * curVal.quantity;
+    return acc + itemTotalPrice;
+  }, 0);
+
+  
   return (
     <div className="container">
       <section className="section-left">
         {myOrderedArray.map(e => {
-          const setQuantity = lastQuantity => {
-            const quantity = lastQuantity - e.quantity;
-            for (let i = 1; i <= quantity; i++) props.dispatch(addToCart(e));
-          };
+          const handleRemove = () => {
+            props.dispatch(removeFromCart(e.id));
+          }
+
+          const setQuantity = quantity => {
+            quantity = parseInt(quantity);
+            handleRemove()
+
+            for (let i = 1; i <= quantity; i++) {
+              props.dispatch(addToCart(e));
+            }
+          };          
+
           return (
             <div
               className="card mb-3"
               style={{ maxWidth: 540 + "px", maxHeight: 300 + "px" }}
               key={e.id}
             >
-              <QuantityForm quantity={e.quantity} setQuantity={setQuantity} />
-
               <div className="row no-gutters">
                 <div className="col-md-8">
                   <div className="card-body">
                     <h5 className="card-title">{e.name}</h5>
-                    <h4>£{e.price}</h4>
+                    <div className="bottom">
+                      <QuantityForm
+                        quantity={e.quantity}
+                        setQuantity={setQuantity}
+                      />
+                      <button className="btn-delete" onClick={handleRemove}>Delete</button>
+                      </div>
+                      <h4>£{e.price}</h4>
                   </div>
                 </div>
               </div>
             </div>
           );
         })}
+      </section>
+      <section className="section-right">
+        <h2>
+          Subtotal ({myOrderedArray.length} Item(s) / {props.data.length}{" "}
+          piece(s)):
+        </h2>
+        <p>£{totalPrice}</p>
+        <BuyNow />
       </section>
     </div>
   );
--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -3,7 +3,14 @@ const initialState = []
 export default (state = initialState, action) => {
   switch (action.type) {
     case "ADD_TO_CART": {
-        return state = [ ...state, action.item]
+        if(state.indexOf(action.item) === -1){
+          action.item.quantity = 1
+          return [...state, action.item]
+        } else {
+          const array = state.filter(e => e.id === action.item.id)
+          action.item.quantity = array.length + 1
+          return state = [ ...state, action.item]
+        }
       }
     case 'REMOVE_FROM_CART': {
         return state.filter(({ id }) => id !== action.id);   
