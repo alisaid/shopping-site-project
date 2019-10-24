5eb3be807720a7838518e4019e03d127023d5cf4
alisaid <alisaid@hotmail.com>
2019-10-24 12:46:42 +0100

    link added to carousel

--- src/components/CarouselContainer.js
+++ src/components/CarouselContainer.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 const CarouselContainer = props => {
   const [index, setIndex] = useState(0);
@@ -22,14 +23,16 @@ const CarouselContainer = props => {
           >
             {props.data.carouselData.map(e => (
               <Carousel.Item key={e.id}>
-                <img
-                  className="d-block w-100"
-                  src={e.image}
-                  alt={e.description}
-                />
-                <Carousel.Caption>
-                  <h3>{e.description}</h3>
-                </Carousel.Caption>
+                <Link to={`/category/${e.id}`} className="btn">
+                  <img
+                    className="d-block w-100"
+                    src={e.image}
+                    alt={e.description}
+                  />
+                  <Carousel.Caption>
+                    <h3>{e.description}</h3>
+                  </Carousel.Caption>
+                </Link>
               </Carousel.Item>
             ))}
           </Carousel>
